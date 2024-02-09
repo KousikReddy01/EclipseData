6852113d40bd83145eae1cac510b787fbe07b4b1
KousikReddy01 <141042551+KousikReddy01@users.noreply.github.com>
2024-02-09 18:39:57 +0530

    Create FibanocciSeries.java

---
+++ src/FibanocciSeries.java
@@ -0,0 +1,14 @@
+public class FibanocciSeries{
+
+  public static void main(String[] args){
+    int i = 0;
+    int b = 1;
+    int c ;
+    for(int i; i<10; i++){
+
+      c = a+b;
+      System.out.println(c);
+      
+    }
+  }
+}
